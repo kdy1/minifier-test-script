64577c907a614689ba2969c666b9d2f1584eeee4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-03 15:02:02 +0900

    Let's try

--- try.mjs
+++ try.mjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import * as  path from 'path';
+import { promises as fs } from 'fs';
+import * as child_process from 'child_process';
 
 console.log('Testing swc minifier');
 
@@ -29,3 +31,19 @@ const optionNames = [
 
 const configJsonPath = 'swc-compress.json';
 
+
+async function tryOption(description, option) {
+    console.log(`Testing: ${description}`);
+
+    await fs.writeFile(configJsonPath, JSON.stringify(option));
+
+    const output = await child_process.exec(`npm run build`);
+}
+
+// Try disabling one option at a time.
+for (const optionName of optionNames) {
+    await tryOption(`without ${optionName}`, {
+        defaults: true,
+        [optionName]: false,
+    })
+}
